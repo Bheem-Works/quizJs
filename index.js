0edaa8f3312730090b461028910ae0141da301df
bheem-works <shokokomichan098@gmail.com>
2025-06-19 09:06:58 +0545

    QuizByJs

---
+++ index.js
@@ -0,0 +1,4 @@
+// Importing all the elemtns class names and storing to the varaibles; 
+// Let's create everything's by using the javascirpt. hahaha 
+
+const questionsCounter = document.querySelector(".questionCounter");
