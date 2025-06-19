871c50acd56ed6dcbb18ddec3ff4eca729ae0eeb
bheem-works <shokokomichan098@gmail.com>
2025-06-19 09:14:33 +0545

    questionsBankCreated

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-// Importing all the elemtns class names and storing to the varaibles; 
+// Importing all the elements class names and storing to the varaibles; 
 // Let's create everything's by using the javascirpt. hahaha 
 
 const questionsCounter = document.querySelector(".questionCounter");
+const questions = document.querySelector(".questions");
+const options = document.querySelector("options");
+// You have to create a buttons with your self. hehe; 
+
+
+// Let's makes some questions through the arrays. 
+const questionBank = [
+  {questions:"What is the color of the sky?",
+  questionsOption : ["sky","blue","yaamamoor"],
+  answer:"blue"
+  },
+  {
+  questions:"Who is the mc of the horimiya?",
+  questionsOption:["hori","izumi","sota","snegokun"],
+  answer:"hori"
+  },
+  {
+    questions:"What is the most hated o/s?",
+    questionsOption:["window","linux","mac"],
+    answer:"window"
+  }
+]
+
