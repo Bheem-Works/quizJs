f19d04e4c46d41da88c1c0029b8128581764a67e
bheem-works <shokokomichan098@gmail.com>
2025-06-19 18:00:33 +0545

    finished quiz Next is sound one This will be funn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const questionsCounter = document.querySelector(".questionCounter");
 const questions = document.querySelector(".questions");
 const options = document.querySelector(".options");
+const finalResult = document.querySelector(".result");
 // You have to create a buttons with your self. hehe; 
 
 
@@ -28,28 +29,56 @@ const questionBank = [
 // creating a chaning input varaibles for store the score and the currentQuestions; 
 let currentQuestions = 0; 
 let score = 0; 
-
-// Questions Count;
 const questionText = document.createElement('p');
-questionText.textContent = `Questions:${score}`; // Use the templete String to get the question numbers value; 
 questionText.style.color = "white";
 questionText.style.fontSize = "20px";
 questionsCounter.appendChild(questionText);
-
 
 // Displaying the questions Bank to the questions varaibles
 // by using the functions of const showOutput; 
 
 function showQuestions() {
+  // Question Counter sections; 
+questionText.textContent = `Questions:${currentQuestions + 1}`; // Use the templete String to get the question numbers value; 
+
+  // Qeustions show; 
   const questionsShow = questionBank[currentQuestions];
   questions.innerText = questionsShow.questions;
-  // options.innerHTML = " ";
+  options.innerHTML = " ";
 
+  // Options show;
   questionsShow.questionsOption.forEach(option=> {
     const buttonElement = document.createElement("button");
+    buttonElement.classList.add("selectButton");
     buttonElement.innerText = option;
    options.appendChild(buttonElement);
-    // buttonElement.addEventListener("click",selectOptions);
+    buttonElement.addEventListener("click",selectOptions);
   })
+}
+function selectOptions(e) {
+  const selected = e.target; 
+  const answer = questionBank[currentQuestions].answer;
+  console.log("answer",answer);
+  if(selected.innerText === answer){
+    score++;
+  }
+    currentQuestions++;
+
+  if(currentQuestions < questionBank.length){
+    showQuestions();
+  } else {
+    showResult();
+  }
+}
+
+function showResult() {
+  questions.innerHTML = "";
+  options.innerHTML = "";
+  questionText.innerHTML = "";
+  const result = document.createElement('p');
+  result.innerHTML = `<h1>Quiz completed </h1>
+    <p>You score : ${score}/${questionBank.length} </p>
+    <p>Bye! Sayanaro </p>`;
+finalResult.appendChild(result);
 }
 showQuestions();
